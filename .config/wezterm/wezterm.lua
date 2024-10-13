1fd3249eafcafb1914137c19ea2d088e73ea865a
Daan Vos de Wael <daan@vosdewael.com>
2024-10-13 14:36:10 +0200

    feat: add formatting of the tab bar and status + keybind for `ClearScrollback`

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,23 +3,22 @@ local mux = wezterm.mux
 local config = wezterm.config_builder()
 
 -- Window
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
+config.window_background_opacity = 0.9
+config.window_decorations = "RESIZE"
+config.window_padding = { left = 100, right = 100, top = 50, bottom = 50 }
 
-wezterm.on('gui-startup', function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():toggle_fullscreen()
+wezterm.on("gui-startup", function(cmd)
+	local _tab, _pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():toggle_fullscreen()
 end)
 
 -- Underlines
-config.underline_thickness = 3
 config.underline_position = -6
+config.underline_thickness = 3
 
 -- Cursor
 config.default_cursor_style = "SteadyBar"
-config.window_padding = { left = 100, right = 100, top = 50, bottom = 50 }
 
 -- Fonts
 config.font = wezterm.font({ family = "Fira Code" })
@@ -27,13 +26,63 @@ config.font_size = 22
 
 -- Color theme
 function scheme_for_appearance(appearance)
-  if appearance:find "Dark" then
-    return "Catppuccin Macchiato"
-  else
-    return "Catppuccin Latte"
-  end
+	if appearance:find("Dark") then
+		return "Catppuccin Macchiato"
+	else
+		return "Catppuccin Latte"
+	end
 end
 
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+
+-- Tab bar
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+config.tab_and_split_indices_are_zero_based = true
+config.tab_bar_at_bottom = true
+config.tab_max_width = 60
+config.use_fancy_tab_bar = false
+
+local function get_current_working_dir(tab)
+	local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = "" }
+	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+
+	return current_dir == HOME_DIR and "."
+	or string.gsub(current_dir.file_path, "(.*[/\\])(.*)", "%2")
+end
+
+wezterm.on("format-tab-title", function(tab)
+	local index = tonumber(tab.tab_index)
+	local custom_title = tab.tab_title
+	local title = get_current_working_dir(tab)
+
+	if custom_title and #custom_title > 0 then
+		title = custom_title
+	end
+
+	return string.format(" %s: %s ", index, title)
+end)
+
+wezterm.on("update-status", function(window)
+	local color_scheme = window:effective_config().resolved_palette
+	local bg = color_scheme.background
+	local fg = color_scheme.foreground
+	local date = wezterm.strftime("%a %-d %b %H:%M");
+
+	window:set_right_status(wezterm.format({
+		{ Background = { Color = bg } },
+		{ Foreground = { Color = fg } },
+		{ Text = " " .. date },
+	}))
+end)
+
+-- Keys
+config.keys = {
+	{
+		key = "k",
+		mods = "CMD", action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+	},
+}
 
 return config
